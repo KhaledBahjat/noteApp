9c7d29e6913d9ee192a8f2bf61d27da81f83830e
Khaled Bahjat <khaledbahjat0@gmail.com>
2025-12-28 03:18:03 +0200

    added logo

---
+++ lib/screens/auth/widgets/logo_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Logo extends StatelessWidget {
+  const Logo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 70.w,
+        height: 70.h,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(20.r),
+        ),
+        child: Image.asset(
+          'assets/logo.png',
+          height: 50,
+        ),
+      ),
+    );
+  }
+}
