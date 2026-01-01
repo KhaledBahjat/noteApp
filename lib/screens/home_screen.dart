744c0f236836315c6fc95d20c391421e2b710eaa
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-01-01 16:33:24 +0200

    intial category widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          Navigator.of(context).pushNamed('addCategory');
+        },
+        child: Icon(Icons.add),
+      ),
       appBar: AppBar(
+        elevation: 5,
+        title: Text('Home Screen'),
         actions: [
           IconButton(
             onPressed: () async {
@@ -23,8 +32,31 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Home Screen'),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          mainAxisExtent: 160,
+        ),
+
+        children: [
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/categorys.png',
+                    height: 100,
+                  ),
+
+                  Text('Category'),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
