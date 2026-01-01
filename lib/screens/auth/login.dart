690a97be8cbbd09c2c17ccc40306adefa6fe6511
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-01-01 14:16:03 +0200

    google sign in done

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -2,6 +2,7 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:note_app/screens/auth/widgets/coustom_text_form.dart';
 import 'package:note_app/screens/auth/widgets/custom_button.dart';
 import 'package:note_app/screens/auth/widgets/forget_password_widget.dart';
@@ -15,6 +16,13 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -223,7 +231,27 @@ class _LoginState extends State<Login> {
                 ),
                 textColor: Colors.white,
                 color: Colors.red.shade700,
-                onPressed: () {},
+                onPressed: () async {
+                  final GoogleSignInAccount? googleUser = await GoogleSignIn()
+                      .signIn();
+                  if (googleUser == null) {
+                    return;
+                  }
+                  // Obtain the auth details from the request
+                  final GoogleSignInAuthentication? googleAuth =
+                      await googleUser?.authentication;
+
+                  // Create a new credential
+                  final credential = GoogleAuthProvider.credential(
+                    accessToken: googleAuth?.accessToken,
+                    idToken: googleAuth?.idToken,
+                  );
+                  // Once signed in, return the UserCredential
+                  await FirebaseAuth.instance.signInWithCredential(credential);
+                  Navigator.of(
+                    context,
+                  ).pushNamedAndRemoveUntil('home', (route) => false);
+                },
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
